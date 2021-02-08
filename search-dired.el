5aaf2407bbb766fbdf134b34a4f83339b8e7b1c5
dalu <moutong945@outlook.com>
2021-02-08 14:42:22 +0800

    Update copyright year to 2021

--- search-dired.el
+++ search-dired.el
@@ -1,4 +1,6 @@
-;;; search-dired.el --- enhance find-dired -*- lexical-binding: t -*-
+;;; search-dired.el --- Enhance find-dired -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020-2021 dalu
 
 ;; Author: dalu <mou.tong@qq.com>
 ;; Maintainer: dalu <mou.tong@qq.com>
@@ -7,22 +9,20 @@
 ;; Homepage: https://www.github.com/dalugm/search-dired
 ;; Keywords: search, file, dired
 
+;; This file is NOT part of GNU Emacs
 
-;; This file is not part of GNU Emacs
-
-;; This file is free software; you can redistribute it and/or modify
+;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
-
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
